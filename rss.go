5f4c4dc22e0c6c5a49757f5a651c8621733c08b7
X3NO <X3NO@disroot.org>
2024-02-04 13:18:30 +0100

    v1.0.0

--- rss.go
+++ rss.go
@@ -1,63 +1,12 @@
 package main
 
 import (
-	"encoding/xml"
-	"fmt"
-	"io"
 	"log"
-	"net/http"
-	"reflect"
 	"regexp"
+	"strings"
+
+	"github.com/antchfx/xmlquery"
 )
-
-func fetch(url string) ([]byte, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-
-	return io.ReadAll(resp.Body)
-}
-
-func printStruct(s interface{}) {
-	structType := reflect.TypeOf(s)
-
-	if structType.Kind() != reflect.Struct {
-		fmt.Println("Input is not a struct.")
-		return
-	}
-
-	for i := 0; i < structType.NumField(); i++ {
-		field := structType.Field(i)
-		fmt.Printf("Field Name: %s\n", field.Name)
-		fmt.Printf("Field Type: %s\n", field.Type)
-		fmt.Printf("Field Tag: %s\n", field.Tag)
-		fmt.Println("--------")
-	}
-}
-
-func printStructField(field reflect.StructField) {
-	fmt.Printf("Field Name: %s\n", field.Name)
-	fmt.Printf("Field Type: %s\n", field.Type)
-	fmt.Printf("Field Tag: %s\n", field.Tag)
-	fmt.Println("--------")
-}
-
-func printType(t reflect.Type) {
-	fmt.Printf("Type Name: %s\n", t.Name())
-	fmt.Printf("Type Kind: %s\n", t.Kind())
-
-	// If it's a struct type, print the fields
-	if t.Kind() == reflect.Struct {
-		for i := 0; i < t.NumField(); i++ {
-			field := t.Field(i)
-			fmt.Printf("Field Name: %s\n", field.Name)
-			fmt.Printf("Field Type: %s\n", field.Type)
-			fmt.Printf("Field Tag: %s\n", field.Tag)
-			fmt.Println("--------")
-		}
-	}
-}
 
 /*
 Get all the new items since the last message from the RSS feed
@@ -69,42 +18,22 @@ Returns:
 	[]string: The items in the RSS feed
 */
 func ParseRss(config *BotConfig) ([]string, error) {
-	rss, err := fetch(config.RssURL)
+	doc, err := xmlquery.LoadURL(config.RssURL)
 	if err != nil {
 		return nil, err
 	}
 
-	var sf []reflect.StructField
-
-	log.Println("formatting:", config.Formatting)
 	formats := regexp.MustCompile(`%([\w>]*)%`).FindAllStringSubmatch(config.Formatting, -1)
 	log.Println("formats:", formats)
-	for i, f := range formats {
-		field := reflect.StructField{Name: fmt.Sprintf("Field_%d", i), Type: reflect.TypeOf(""), Tag: reflect.StructTag(fmt.Sprintf("`xml:\"%s\"`", f[1]))}
-		sf = append(sf, field)
+	
+	var posts []string
+	for _, item := range xmlquery.Find(doc, "//item") {
+		var msg string = config.Formatting
+		for _, f := range formats {
+			msg = strings.ReplaceAll(msg, f[0], xmlquery.FindOne(item, f[1]).InnerText())
+		}
+		posts = append(posts, msg)
 	}
 
-	for i, f := range sf {
-		log.Println("field:", i)
-		printStructField(f)
-	}
-
-	st := reflect.StructOf(sf)
-
-	log.Println("type")
-	printType(st)
-
-	s := reflect.New(st)
-
-	log.Println("struct")
-	printStruct(s) // ERROR: what the fuck????
-
-	err = xml.Unmarshal(rss, &s)
-	if err != nil {
-		return nil, err
-	}
-
-	log.Printf("struct: %v\n", s)
-
-	return nil, nil		
+	return posts, nil
 }
